f813fc9f53d0e43158b8ec8fcf1b6842b4c2f48f
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2024-07-05 15:52:20 -0300

    health check

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import express, { json } from "express";
+import express, { json, Request, Response } from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
 
 import contactRouter from "./routes/contacts-router";
 import errorHandlerMiddleware from "./middlewares/error-middleware";
+import httpStatus from "http-status";
 
 dotenv.config();
 
 const app = express();
+app.get("/health", (req: Request, res: Response) => res.sendStatus(httpStatus.OK));
 app.use(json());
 app.use(contactRouter);
 app.use(errorHandlerMiddleware);
