88eef9a3e96c56090febda303cea4fab0cf47a7b
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2024-07-05 15:39:00 -0300

    fix: repositories

--- src/repositories/contacts-repository.ts
+++ src/repositories/contacts-repository.ts
@@ -1,55 +1,34 @@
 import prisma from "../database";
-import { contacts, phones } from "@prisma/client";
 import { CreateContactData } from "../protocols";
 
-type ContactWithPhones = contacts & {
-  phones: phones
-}
-
 export async function selectAllContacts() {
-  const contacts = await prisma.contacts.findMany();
-
-  const contactsWithPhones: ContactWithPhones[] = [];
-
-  for (const contact of contacts) {
-    const phones = await prisma.phones.findFirst({
-      where: {
-        contact_id: contact.id
-      }
-    })
-
-    contactsWithPhones.push({
-      ...contact, phones
-    })
-  }
+  const contacts = await prisma.contact.findMany({
+    include: {
+      phones: true
+    }
+  });
 
   return contacts;
 }
 
 export async function insertContact(contactData: CreateContactData) {
-  const { id: contactId } = await createContact(contactData);
-  await savePhonesFromContact(contactId, contactData.phones);
-}
-
-async function createContact(contactData: CreateContactData) {
-  const contact = await prisma.contacts.create({
+  const contact = await prisma.contact.create({
     data: {
       fullname: contactData.fullname,
       email: contactData.email,
-      picture: contactData.picture
+      picture: contactData.picture,
+      phones: {
+        createMany: {
+          data: contactData.phones.map(phone => {
+            return {
+              number: phone
+            }
+          })
+        }
+      }
+
     }
   });
 
   return contact;
-}
-
-async function savePhonesFromContact(contactId: number, phones: string[]) {
-  await prisma.phones.createMany({
-    data: phones.map(phone => {
-      return {
-        contact_id: contactId,
-        number: phone
-      }
-    })
-  });
 }
